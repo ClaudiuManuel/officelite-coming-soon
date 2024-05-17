e28ba2bee63151f5a82aa20e7ae7a688411b148f
ClaudiuManuel <calian.claudiu@yahoo.com>
2024-05-17 18:35:52 +0300

    Dropdown animations + hover effect

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -3,6 +3,7 @@ import arrow from '@assets/sign-up/arrow.svg';
 import tick from '../assets/sign-up/tick.svg';
 import { cn } from '../utils/tailwindUtilities';
 import { DropdownProps } from '@/types';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Dropdown: React.FC<DropdownProps> = ({ options, selectedOptionIndex, setSelectedOptionIndex }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -18,37 +19,52 @@ const Dropdown: React.FC<DropdownProps> = ({ options, selectedOptionIndex, setSe
                     {options[selectedOptionIndex].name}{' '}
                     <span className="ml-2 text-lightGray">{options[selectedOptionIndex].price}</span>
                 </span>
-                <img src={arrow} />
+                <img
+                    src={arrow}
+                    className={cn('duration-400 transition-transform', {
+                        'rotate-180': isOpen,
+                    })}
+                />
             </button>
 
-            {isOpen && (
-                <div className="relative">
-                    <div className="absolute -top-4 left-0 z-50 flex w-full flex-col rounded-lg border border-gray-700 border-opacity-20 bg-white py-2 pl-8 pr-8 shadow">
-                        <ul>
-                            {options.map((option, index) => (
-                                <li
-                                    onClick={() => {
-                                        setSelectedOptionIndex(index);
-                                        setIsOpen(false);
-                                    }}
-                                    className={cn(
-                                        'flex cursor-pointer flex-row items-baseline justify-between  py-4 pr-3 font-KumbhBold text-dark',
-                                        {
-                                            'border-b border-b-lightGray/25': index !== options.length - 1,
-                                        }
-                                    )}
-                                >
-                                    <span>
-                                        {option.name}
-                                        <span className="ml-2 text-lightGray"> {option.price} </span>
-                                    </span>
-                                    {index === selectedOptionIndex && <img src={tick} />}
-                                </li>
-                            ))}
-                        </ul>
+            <AnimatePresence initial={false}>
+                {isOpen && (
+                    <div className="relative">
+                        <motion.div
+                            initial={{ opacity: 0, y: -20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 0 }}
+                            transition={{ duration: 0.3 }}
+                            className="absolute z-50 flex w-full flex-col rounded-lg border border-gray-700 border-opacity-20 bg-white py-2 pl-8 pr-8 shadow"
+                        >
+                            <ul>
+                                {options.map((option, index) => (
+                                    <li
+                                        onClick={() => {
+                                            setSelectedOptionIndex(index);
+                                            setIsOpen(false);
+                                        }}
+                                        className={cn(
+                                            'group flex cursor-pointer flex-row items-baseline  justify-between py-4 pr-3 font-KumbhBold text-dark hover:text-accentBlue',
+                                            {
+                                                'border-b border-b-lightGray/25': index !== options.length - 1,
+                                            }
+                                        )}
+                                    >
+                                        <span>
+                                            {option.name}
+                                            <span className="ml-2 text-lightGray group-hover:text-accentBlue">
+                                                {option.price}
+                                            </span>
+                                        </span>
+                                        {index === selectedOptionIndex && <img src={tick} />}
+                                    </li>
+                                ))}
+                            </ul>
+                        </motion.div>
                     </div>
-                </div>
-            )}
+                )}
+            </AnimatePresence>
         </>
     );
 };
