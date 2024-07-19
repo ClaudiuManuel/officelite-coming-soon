f20b239d494e87317b54452ffb3f3743716064b8
ClaudiuManuel <calian.claudiu@yahoo.com>
2024-07-19 19:42:04 +0300

    Retrieve deadline from local storage

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,13 +4,19 @@ import { TimeRemaining } from '@/types';
 import { calculateTimeRemaining } from '@/utils/timeHelpers';
 
 const Countdown = ({ isLightVariant = false }: { isLightVariant?: boolean }) => {
-    // Add 21 days to the current date. Just for showcase purposes.
     const futureDate = useMemo(() => {
-        const newDate = new Date();
-        newDate.setDate(newDate.getDate() + 21);
-        console.log(newDate);
+        const dateFromStorage = window.localStorage.getItem('deadline');
+        
+        if (dateFromStorage) {
+            return new Date(dateFromStorage);
+        } else {
+            // Add 21 days to the current date. Just for showcase purposes.
+            const newDate = new Date();
+            newDate.setDate(newDate.getDate() + 21);
+            window.localStorage.setItem('deadline', newDate.toISOString());
 
-        return newDate;
+            return newDate;
+        }
     }, []);
 
     const [timeRemaining, setTimeRemaining] = useState<TimeRemaining>(calculateTimeRemaining(futureDate));
@@ -31,7 +37,7 @@ const Countdown = ({ isLightVariant = false }: { isLightVariant?: boolean }) => 
             <div className={'flex gap-4'}>
                 {Object.keys(timeRemaining).map((timeUnit, index) => {
                     const stronglyTypedTimeUnit = timeUnit as keyof TimeRemaining;
-                    return (                        
+                    return (
                         <CountdownTile
                             key={index}
                             digitPair={timeRemaining[stronglyTypedTimeUnit].toString()}
