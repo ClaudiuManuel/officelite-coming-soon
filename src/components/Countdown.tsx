0319d1df4cf42c6f6ca38f4c8df8e9ff0a9a3e59
ClaudiuManuel <calian.claudiu@yahoo.com>
2024-08-17 20:32:37 +0300

    countdown title dynamic & added handle for when deadline is passed

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,16 +2,17 @@ import { useEffect, useMemo, useState } from 'react';
 import CountdownTile from './CountdownTile';
 import { TimeRemaining } from '@/types';
 import { calculateTimeRemaining } from '@/utils/timeHelpers';
+import { formatDate } from '@/utils/dateHelpers';
 
 const Countdown = ({ isLightVariant = false }: { isLightVariant?: boolean }) => {
     const futureDate = useMemo(() => {
         const dateFromStorage = window.localStorage.getItem('deadline');
-        
-        if (dateFromStorage) {
+        const newDate = new Date();
+
+        // If the deadline is stored in the local storage and it's not in the past, use it. Otherwise, set a new deadline for showcase purposes.
+        if (dateFromStorage && new Date(dateFromStorage) > newDate) {
             return new Date(dateFromStorage);
         } else {
-            // Add 21 days to the current date. Just for showcase purposes.
-            const newDate = new Date();
             newDate.setDate(newDate.getDate() + 21);
             window.localStorage.setItem('deadline', newDate.toISOString());
 
@@ -32,7 +33,7 @@ const Countdown = ({ isLightVariant = false }: { isLightVariant?: boolean }) => 
     return (
         <div className={'flex flex-col gap-4'}>
             <h3 className="text-left">
-                Coming <span className="text-accentBlue">4 NOV 2020</span>
+                Coming <span className="text-accentBlue">{formatDate(futureDate)}</span>
             </h3>
             <div className={'flex gap-4'}>
                 {Object.keys(timeRemaining).map((timeUnit, index) => {
---
+++ src/utils/dateHelpers.ts
@@ -0,0 +1,7 @@
+export const formatDate = (date: Date): string => {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = date.toLocaleString('default', { month: 'short' }).toUpperCase();
+    const year = date.getFullYear();
+
+    return `${day} ${month} ${year}`;
+};
